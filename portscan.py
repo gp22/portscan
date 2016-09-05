47e5a746cd7ba7ae0c3d0be58955a2be7880623a
gp22 <paulgarcia@protonmail.com>
2016-09-05 15:25:51 +0700

    Added scan counter and default port_range variable

--- portscan.py
+++ portscan.py
@@ -13,8 +13,13 @@ parser.add_argument('-r', '--range', nargs=2, type=int,
                     help='port range to scan: [start port] [end port]')
 args = parser.parse_args()
 
+# set host and port_range variables
+# set port_range to (1, 1024) by default
 host = args.host
-port_range = args.range
+if args.range:
+    port_range = args.range
+else:
+    port_range = [1, 1024]
 
 # get the IP address of the host to scan
 hostIP = socket.gethostbyname(host)
@@ -26,6 +31,9 @@ print('-' * 60, '\n')
 # get the starting time
 start_time = time.monotonic()
 
+# initialize scan counter
+scan_count = 0
+
 for p in range(port_range[0], port_range[1] + 1):
     # create the socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
@@ -34,6 +42,7 @@ for p in range(port_range[0], port_range[1] + 1):
     if connect_result == 0:
         print('Port {}: Open'.format(p))
     sock.close()
+    scan_count += 1
 
 # get the ending time
 end_time = time.monotonic()
@@ -41,4 +50,4 @@ end_time = time.monotonic()
 # print results
 duration = end_time - start_time
 duration = round(duration, 2)
-print('\nN ports scanned in {} seconds'.format(duration))
+print('\n{} ports scanned in {} seconds'.format(scan_count, duration))
